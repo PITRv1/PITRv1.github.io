ae23cd0b0f26dc644e15b336e8580d592278d881
PITRv1 <petersandor.toth@gmail.com>
2025-04-18 23:00:58 +0200

    happenings happening

--- JS/mainPage.js
+++ JS/mainPage.js
@@ -1,2 +1,18 @@
 const video = document.getElementById('bg-video');
 video.playbackRate = 0.75;
+
+const start_button = document.getElementById("start-btn")
+
+const gradient_overlay = document.getElementById("gradient-overlay")
+const main_content = document.getElementById("main-content")
+
+
+
+start_button.addEventListener("click", () => {
+    gradient_overlay.classList.remove("hidden");
+    main_content.classList.remove("hidden");
+
+    document.body.classList.remove("noscroll");
+
+    start_button.classList.add("hidden");
+})
